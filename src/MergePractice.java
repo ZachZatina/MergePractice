716e4610fd294f31d3cdbdbda84d4fc93f3f62bd
saclauw <saclauw@gmail.com>
2018-02-07 11:46:23 -0500

    added a new line to the file

--- src/MergePractice.java
+++ src/MergePractice.java
@@ -6,6 +6,8 @@ public class MergePractice {
 	public static void main(String[] args) {
 		System.out.println("Hello World");
 		System.out.println("My name is zach");
+		
+		System.out.println("My name is Steven");
 	}
 	
 }
