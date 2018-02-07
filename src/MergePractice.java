acc5f1dc399cfccd3d3823ef716b6d7d74db8165
ZachZatina <zachzatina327@yahoo.com>
2018-02-07 11:43:53 -0500

    added line at top

--- src/MergePractice.java
+++ src/MergePractice.java
@@ -4,6 +4,7 @@
 public class MergePractice {
 
 	public static void main(String[] args) {
+		System.out.println("Cool new code");
 		System.out.println("Hello World");
 		System.out.println("My name is zach");
 	}
