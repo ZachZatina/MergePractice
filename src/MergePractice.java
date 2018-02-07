8cbf6a52120f356ca5b98371dc2bd507684adf29
ZachZatina <zachzatina327@yahoo.com>
2018-02-07 11:31:00 -0500

    initial commit

---
+++ src/MergePractice.java
@@ -0,0 +1,11 @@
+/*
+ * Zach Zatina
+ */
+public class MergePractice {
+
+	public static void main(String[] args) {
+		System.out.println("Hello World");
+		System.out.println("My name is zach");
+	}
+	
+}
